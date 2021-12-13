c3f4c8d5c14ef9213fe689792926585f9f853f8d
wilson <3050166231@qq.com>
2021-12-14 06:44:32 +0800

    refactor helper.py

--- json_inherit_process/helper.py
+++ json_inherit_process/helper.py
@@ -1,15 +1,71 @@
+import re
+
+
 def separate_value_unit(s: str):
-    d = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", "-"]
-    index = -1
-    for i in range(len(s))[::-1]:
-        if not s[i] in d:
-            index = i
-    return float(s[:index]), s[index:].lstrip()
+    r = "^((\-|\+)?\s*\d+(\.\d+)?)\s*(\S*)$"
+    m = re.search(r, s)
+    return float(m.group(1).replace(" ", "")), m.group(4)
 
 
 def have_number(s: str):
-    d = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", "-"]
-    for i in range(len(s))[::-1]:
-        if s[i] in d:
-            return True
-    return False
+    r = "^(\-|\+)?\s*\d+(\.\d+)?"
+    m = re.search(r, s)
+    return m != None
+
+
+def test():
+    test = "324"
+    assert have_number(test)
+    assert separate_value_unit(test) == (324, "")
+
+
+def test1():
+    test = "324.34"
+    assert have_number(test)
+    assert separate_value_unit(test) == (324.34, "")
+
+
+def test2():
+    test = "-324.34 "
+    assert have_number(test)
+    assert separate_value_unit(test) == (-324.34, "")
+
+
+def test3():
+    test = "- 324.34"
+    assert have_number(test)
+    assert separate_value_unit(test) == (-324.34, "")
+
+
+def test4():
+    test = "- 324.3434"
+    assert have_number(test)
+    assert separate_value_unit(test) == (-324.3434, "")
+
+
+def test5():
+    test = "- 324.3434df"
+    assert have_number(test)
+    assert separate_value_unit(test) == (-324.3434, "df")
+
+
+def test6():
+    test = "- 324.3434 df"
+    assert have_number(test)
+    assert separate_value_unit(test) == (-324.3434, "df")
+
+
+def test7():
+    test = "sfd"
+    assert not have_number(test)
+
+
+if __name__ == "__main__":
+    test()
+    test1()
+    test2()
+    test3()
+    test4()
+    test5()
+    test6()
+    test7()
