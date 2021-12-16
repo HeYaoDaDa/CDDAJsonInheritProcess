86dc7d919581ab77fd26ae14725cb048ed8e078e
wilson <3050166231@qq.com>
2021-12-16 21:21:13 +0800

    add mod_helper

---
+++ json_inherit_process/mod.py
@@ -0,0 +1,13 @@
+class mod:
+    def __init__(self, jsondata: dict, path: str) -> None:
+        self.id: str = jsondata["id"]
+        self.name: str = jsondata["name"]
+        self.path: list[str] = [path]
+        self.dependencies: list[str] = []
+        if "dependencies" in jsondata:
+            json_depe = jsondata["dependencies"]
+            self.dependencies = json_depe if type(
+                json_depe) is list else [json_depe]
+
+    def __str__(self) -> str:
+        return f"(\n  id:{self.id}\n  name:{self.name}\n  path:{self.path}\n  dependencies:{self.dependencies}\n)"
---
+++ json_inherit_process/mod_helper.py
@@ -0,0 +1,62 @@
+import os
+import json
+import copy
+from .mod import mod
+
+
+def __scan_mods(path: str) -> list[mod]:
+    path = os.path.expanduser(path)
+    mods: list[mod] = []
+    files = os.listdir(path)
+    for file in files:
+        file = os.path.join(path, file)
+        if os.path.isdir(file):
+            mods += __scan_mods(file)
+        else:
+            mods += __scan_modinfo(file)
+    return mods
+
+
+def __scan_modinfo(file: str) -> list[mod]:
+    mods: list[mod] = []
+    if (file.endswith("modinfo.json")):
+        with open(file, encoding="utf-8") as fp:
+            jsondata = json.load(fp)
+        if type(jsondata) is list:
+            for jsondata_item in jsondata:
+                if jsondata_item["type"] == "MOD_INFO":
+                    mods.append(mod(jsondata_item, os.path.dirname(file)))
+        elif type(jsondata) is dict:
+            mods.append(mod(jsondata, os.path.dirname(file)))
+    return mods
+
+
+def __find_mod(mods: list[mod], id: str) -> mod:
+    for mod in mods:
+        if id == mod.id:
+            return mod
+
+
+def __sort_mods(mods: list[mod]) -> list[mod]:
+    result: list[mod] = []
+    my_mods: list[mod] = copy.deepcopy(mods)
+    while len(my_mods) > 0:
+        for my_mod in my_mods[::-1]:
+            if len(my_mod.dependencies) == 0:
+                result.append(__find_mod(mods, my_mod.id))
+                my_mods.remove(my_mod)
+        for my_mod in my_mods:
+            for dependen in my_mod.dependencies[::-1]:
+                for result_mod in result:
+                    if dependen == result_mod.id:
+                        my_mod.dependencies.remove(dependen)
+    return result
+
+
+def get_sort_mods(path: str) -> list[mod]:
+    return __sort_mods(__scan_mods(path))
+
+
+if __name__ == "__main__":
+    for my_mod in get_sort_mods("~/Downloads/Cataclysm-DDA-master/data/mods/"):
+        print(my_mod)
